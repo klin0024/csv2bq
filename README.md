e26efdf3b70f3aa540ebea0f0c64478d49ce155e
shuhsien <shuhsien.lin@ikala.tv>
2021-04-16 16:43:47 +0800

    add py

--- README.md
+++ README.md
@@ -24,7 +24,6 @@ python3 csv2bq.py --project=gcp-expert-sandbox-allen \\<br>
 --schema='{"admit":"FLOAT","gre":"FLOAT","gpa":"FLOAT","rank":"FLOAT"}'
 
 
-
 # csv2bq-part.py 參數說明
 
 Var           | Value                                         | Description
@@ -49,3 +48,29 @@ python3 csv2bq-part.py --project=gcp-expert-sandbox-allen \\<br>
 --input=gs://gcp-expert-sandbox-allen/folder/data.csv \\<br>
 --output=gcp-expert-sandbox-allen:dataset.table \\<br>
 --schema='{"admit":{"enabled":1,"type":"FLOAT"},"gre":{"enabled":1,"type":"FLOAT"},"gpa":{"enabled":0,"type":"FLOAT"},"rank":{"enabled":1,"type":"FLOAT"}}'
+
+
+# csv2bq-part.py 參數說明
+
+Var           | Value                                         | Description
+:-------------|:----------------------------------------------|:------------------------
+--project	  | gcp-expert-sandbox-allen	                  | 專案名稱
+--region	  | us-central1	                                  | 地區名稱
+--credential  | gcp-expert-sandbox-allen-c1fcfd19238a.json	  | 憑據檔案名稱
+--temp_bucket | gcp-expert-sandbox-allen-temp_bucket	      | 暫存bucket名稱
+--input	      | gs://gcp-expert-sandbox-allen/folder/data.csv | 輸入的CSV位置
+--output	  | gcp-expert-sandbox-allen:dataset.table	      | 輸出bq的表
+--schema	  | '[{"mode":"NULLABLE","name":"admit","type":"FLOAT"},<br>{"mode":"NULLABLE","name":"gre","type":"FLOAT"},<br>{"mode":"NULLABLE","name":"gpa","type":"FLOAT"},<br>{"mode":"NULLABLE","name":"rank","type":"FLOAT"}]' | schema json,可以選擇欄位是否要輸出到bq
+--skip_csv_lines | 1                                       | Default: 1 , 忽略csv的行數
+
+# csv2bq-schema.py 使用說明
+
+pip3 install -r requirements.txt
+
+python3 csv2bq-schema.py --project=gcp-expert-sandbox-allen \\<br>
+--region=us-central1 \\<br>
+--credential=gcp-expert-sandbox-allen-c1fcfd19238a.json \\<br>
+--temp_bucket=gcp-expert-sandbox-allen-temp_bucket \\<br>
+--input=gs://gcp-expert-sandbox-allen/folder/data.csv \\<br>
+--output=gcp-expert-sandbox-allen:dataset.table \\<br>
+--schema="$(generate-schema --input_format=csv < data.csv)"
---
+++ csv2bq-schema.py
@@ -0,0 +1,124 @@
+from __future__ import absolute_import
+import argparse
+import logging
+import re
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+import os
+import json
+from apache_beam.io.gcp.bigquery_tools import parse_table_schema_from_json
+
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "gcp-expert-sandbox-allen-c1fcfd19238a.json"
+
+class DataIngestion:
+
+    def __init__(self,schema_string): 
+        self.schema = [ s['name'] for s in json.loads(schema_string) ]
+
+    def parse_method(self, string_input):
+        values = re.split(",",
+                          re.sub('\r\n', '', re.sub(u'"', '', string_input)))
+        row = dict(
+            zip(self.schema,
+                values))
+        return row
+
+
+def run(argv=None):
+    """The main function which creates the pipeline and runs it."""
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--input',
+        dest='input',
+        required=True,
+        help='Input file to read. This can be a local file or '
+        'a file in a Google Storage Bucket.')
+
+    # This defaults to the lake dataset in your BigQuery project. You'll have
+    # to create the lake dataset yourself using this command:
+    # bq mk lake
+    parser.add_argument('--output',
+                        dest='output',
+                        required=True,
+                        help='Output BQ table to write results to.')
+
+
+    parser.add_argument('--temp_bucket',
+                        dest='temp_bucket',
+                        required=True,
+                        help='temp bucket name.')
+
+
+    parser.add_argument('--schema',
+                        dest='schema_string',
+                        required=True,
+                        help='data schema json format.')
+    
+
+    parser.add_argument('--credential',
+                        dest='credential',
+                        required=True,
+                        help='credential json key.')
+
+
+    parser.add_argument('--skip_csv_lines',
+                        dest='skip_csv_lines',
+                        type=int,
+                        required=False,
+                        help='skip csv lines.',
+                        default=1)
+
+    # Parse arguments from the command line.
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    pipeline_args += ["--runner=DataflowRunner", 
+                      "--save_main_session", 
+                      #"--staging_location=gs://%s/staging" % (known_args.temp_bucket),
+                      "--temp_location=gs://%s/temp" % (known_args.temp_bucket)]
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = known_args.credential
+
+    schema = parse_table_schema_from_json('{"fields":%s}' % (known_args.schema_string))
+
+    # DataIngestion is a class we built in this script to hold the logic for
+    # transforming the file into a BigQuery table.
+    data_ingestion = DataIngestion(known_args.schema_string)
+
+    # Initiate the pipeline using the pipeline arguments passed in from the
+    # command line. This includes information such as the project ID and
+    # where Dataflow should store temp files.
+    p = beam.Pipeline(options=PipelineOptions(pipeline_args))
+
+    (
+     p | 'Read from a File' >> beam.io.ReadFromText(known_args.input,
+                                                  skip_header_lines=known_args.skip_csv_lines)
+    
+     # This stage of the pipeline translates from a CSV file single row
+     # input as a string, to a dictionary object consumable by BigQuery.
+     # It refers to a function we have written. This function will
+     # be run in parallel on different workers using input from the
+     # previous stage of the pipeline.
+     | 'String To BigQuery Row' >>
+     beam.Map(lambda s: data_ingestion.parse_method(s))
+     | 'Write to BigQuery' >> beam.io.Write(
+         beam.io.BigQuerySink(
+             # The table name is a required argument for the BigQuery sink.
+             # In this case we use the value passed in from the command line.
+             known_args.output,
+             # Here we use the simplest way of defining a schema:
+             # fieldName:fieldType
+
+             schema=schema,
+
+             # Creates the table in BigQuery if it does not yet exist.
+             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+             # Deletes all data in the BigQuery table before writing.
+             write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)))
+    p.run().wait_until_finish()
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
